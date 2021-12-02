1ec5e8612071682eebd5274a8d7b266160ae54b7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-12-02 17:24:26 +0100

    Solve day 1 with javascript
    To run the tests (Thanks to Riki) I have to run this:
      node --experimental-vm-modules node_modules/jest/bin/jest.js

--- 2021/day-1/sonar.js
+++ 2021/day-1/sonar.js
@@ -1,21 +1,23 @@
 function eachCons(arr, n) {
   let acc = []
 
-  for (let i = 0; i < arr.length - n; i++) {
-    acc.push(arr.slice(idx, idx + n))
+  for (let i = 0; i <= arr.length - n; i++) {
+    acc.push(arr.slice(i, i + n))
   }
 
   return acc
 }
 
 function sum(arr) {
-  arr.reduce((a, e) => a + e, 0)
+  return arr.reduce((a, e) => a + e, 0)
 }
 
 export function sonar(movingAvg = 1) {
   return (measurements) => {
-    let noise_reduced_measurements = eachCons((measurements, movingAvg)).map(sum)
+
+    let noise_reduced_measurements = eachCons(measurements, movingAvg).map(sum)
     return eachCons(noise_reduced_measurements, 2)
-      .filter(({ a, b }) => b > a).length
+      .filter(([a, b]) => b > a)
+      .length
   }
 }
--- 2021/day-1/sonar.test.js
+++ 2021/day-1/sonar.test.js
@@ -2,7 +2,7 @@ import { sonar } from './sonar.js'
 
 const measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 test('Counts the increases (simple)', () => {
-  expect(sonar()(measurements)).toBe(7);
+  expect(sonar(1)(measurements)).toBe(7);
 });
 
 test('Counts the increases (rolling window)', () => {
