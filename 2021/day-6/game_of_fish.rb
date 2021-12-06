4b52ede260454c48380a9c4a1064ff234022bec7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-12-06 07:48:15 +0100

    Day 6 first task

---
+++ 2021/day-6/game_of_fish.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def fish_population(initial_state, iterations)
+  # puts "Initial state: #{initial_state}"
+
+  iterations.times.each_with_object(initial_state) do |_i, state|
+    new_additions = []
+    state.map! do |e|
+      if e > 0
+        e - 1
+      else
+        new_additions << 8
+        6
+      end
+    end
+    state.push(*new_additions)
+
+    # puts "After #{i + 1} days: #{state}"
+  end.size
+end
+
+if __FILE__ == $PROGRAM_NAME
+  input = File.read(File.expand_path('./input.txt', __dir__))
+  initia_state = input.split(',').map(&:to_i)
+  puts fish_population(initia_state, 80)
+end
---
+++ 2021/day-6/game_of_fish_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative './game_of_fish'
+
+class GameOfFishTest < Minitest::Test
+  def setup
+    @input = [3, 4, 3, 1, 2]
+  end
+
+  def test_iteration_after_18_days
+    assert_equal 26, fish_population(@input, 18)
+  end
+
+  def test_iteration_after_80_days
+    assert_equal 5934, fish_population(@input, 80)
+  end
+end
